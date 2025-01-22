2148b1f5d0bedf0ef83be90c281362d47dcd688e
¨lmriofrio¨ <lmriofrio@utpl.edu.ec>
2025-01-22 01:45:06 -0500

    pre act9 1.0.1

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: 'localhost',
+  host: '64.23.134.120',
   port: 3306,
   username: 'lmriofrio',
   password: 'lmriofrio',
